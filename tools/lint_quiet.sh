020bf76f2db0daffb210e850833de1808e105208
k-matsuzawa <matsuzawa@cryptogarage.co.jp>
2019-12-16 17:41:15 +0900

    update cpplint

--- tools/lint_quiet.sh
+++ tools/lint_quiet.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
+lint_ecode=0
+
 function search_lint() {
     cd $2
     for file in `\find . -maxdepth 1 -name '*.h'`; do
         $1 $3/tools/cpplint/cpplint.py --quiet $file > /dev/null
+        if [ $? -gt 0 ]; then
+            lint_ecode=1
+        fi
     done
     for file in `\find . -maxdepth 1 -name '*.cpp'`; do
         $1 $3/tools/cpplint/cpplint.py --quiet $file > /dev/null
+        if [ $? -gt 0 ]; then
+            lint_ecode=1
+        fi
     done
     cd $3
 }
@@ -22,3 +30,7 @@ search_lint $py_exe include/cfd ../..
 search_lint $py_exe include/cfdc ../..
 search_lint $py_exe src ..
 search_lint $py_exe src/capi ../..
+
+if [ $lint_ecode -gt 0 ]; then
+    exit 1
+fi
