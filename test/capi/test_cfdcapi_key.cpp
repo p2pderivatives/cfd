6a0d8c023862394add7e3ec8057fc6dc5e88bdf2
fujita-cg <fujita@cryptogarage.co.jp>
2020-01-08 17:04:59 +0900

    fix test data

--- test/capi/test_cfdcapi_key.cpp
+++ test/capi/test_cfdcapi_key.cpp
@@ -91,15 +91,19 @@ TEST(cfdcapi_key, CfdNormalizeSignatureTest) {
   EXPECT_EQ(kCfdSuccess, ret);
   EXPECT_FALSE((NULL == handle));
 
+  // FIXME(fujita-cg): replace test data
+  // this test data comes from https://www.pebblewind.com/entry/2018/04/27/232427
   if (ret == kCfdSuccess) {
-    const char* signature = "773420c0ded41a55b1f1205cfb632f08f3f911a53e7338a0dac73ec6cbe3ca471907434d046185abedc5afddc2761a642bccc70af6d22b46394f1d04a8b24226";
+    const char* signature = "c6047f9441ed7d6d3045406e95c07cd85c778e4b8cef3ca7abac09b95c709ee5f67f6cf81a19873091aa7c9578fa2e96490e9bfc78ae7e9798004e8252c06287";
     char* normalized;
     ret = CfdNormalizeSignature(handle, signature, &normalized);
     EXPECT_EQ(kCfdSuccess, ret);
-    // equals data on generate by libwally
-    EXPECT_STREQ(normalized, signature);
-    CfdFreeStringBuffer(normalized);
-    normalized = NULL;
+    const char* expect = "c6047f9441ed7d6d3045406e95c07cd85c778e4b8cef3ca7abac09b95c709ee509809307e5e678cf6e55836a8705d16871a040ea369a21a427d2100a7d75deba";
+    EXPECT_STREQ(normalized, expect);
+    if (ret == kCfdSuccess) {
+      CfdFreeStringBuffer(normalized);
+      normalized = NULL;
+    }
   }
 
   ret = CfdGetLastErrorCode(handle);
